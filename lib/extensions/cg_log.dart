778e63c5c3fb8d081e95faf530740ecf1f385112
amospdev <amos.p.dev@gmail.com>
2023-10-26 14:57:21 +0300

    add new logger

---
+++ lib/extensions/cg_log.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+class Logger {
+  void logDev(String log) {
+    if (kDebugMode) {
+      print(log);
+    }
+  }
+}
+
+void logDebug(String log) {
+  if (kDebugMode) {
+    print(log);
+  }
+}
